445dd2d30a618a4e92775c2f3957cd81948eca28
Sadee <mohammadsadee24@gmail.com>
2022-09-11 18:26:00 +0600

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * search bar toggle
+ */
+
+const searchBar = document.querySelector("[data-search-bar]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleSearchBar = function () {
+  searchBar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(searchTogglers, "click", toggleSearchBar);
